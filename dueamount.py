8450b8c8ed9487c1c5f4cc0d5c13c7ab869e4564
XxShayanxxX <balajishayan@gmail.com>
2024-11-19 19:36:05 +0800

    first commit

---
+++ dueamount.py
@@ -0,0 +1,4 @@
+amountdue = int(input("Enter the amount in the recipt :"))
+amountpaid = int(input("Enter how much you money you gave :"))
+
+print("The amount the shopkeepers owes to you is : $", amountpaid - amountdue)
